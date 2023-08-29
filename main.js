3d5f3b4d4031bed2b2847240cd95be9ff2fb3e95
Axil <egorka.tretyakov.2017@gmail.com>
2023-08-29 22:31:43 +0300

    feat: realized operation '%'

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let firstOperand = '',
-    secondOperand = '',
-    sign = '',
-    finish = false;
+let firstOperand = '',  // first operand
+    secondOperand = '', // second operand
+    sign = '',          // sign
+    finish = false;     // calculation completed or not
 
 const arrOfDigits = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const arrOfSigns = ['-', '+', 'X', '/'];
+const arrOfSigns = ['-', '+', 'X', '/', '%'];
 
 const resultElement = document.querySelector('.calc-screen p');
 
@@ -16,45 +16,53 @@ let clearAll = () => {
     resultElement.textContent = '0';
 }
 
+// the button 'ac' was pressed
 document.querySelector('.ac').onclick = clearAll;
 
 document.querySelector('.buttons').onclick = (event) => {
+    // the button wasn't pressed
     if (!event.target.classList.contains('btn')) {
         return;
     }
+    // the button 'ac' was pressed
     if (event.target.classList.contains('ac')) {
         return;
     }
 
+    // get pressed button
     const key = event.target.textContent;
 
+    // if the button is a number or '.'
     if (arrOfDigits.includes(key)) {
+        // enter the first value
         if (secondOperand === '' && sign === '') {
             firstOperand += key;
             resultElement.textContent = firstOperand;
-        } else if (firstOperand !== '' && secondOperand !== '' && finish) {
+        } // enter the second value (if the result is calculated)
+          else if (firstOperand !== '' && secondOperand !== '' && finish) {
           secondOperand = key;
           finish = false;
           resultElement.textContent = secondOperand;
-        } else {
+        } // enter the second value (if the result isn't calculated)
+          else {
             secondOperand += key;
             resultElement.textContent = secondOperand;
         }
         console.log(firstOperand, secondOperand, sign);
         return;
     }
-
+    // if the button is a sign
     if (arrOfSigns.includes(key)) {
         sign = key;
         console.log(firstOperand, secondOperand, sign);
         resultElement.textContent = sign;
     }
-
+    // if the button is a '='
     if (key === '=') {
+        // press equal after the operator
         if (secondOperand === '') {
             secondOperand = firstOperand;
         }
-
         switch (sign) {
             case '+':
                 firstOperand = +firstOperand + +secondOperand;
@@ -74,6 +82,9 @@ document.querySelector('.buttons').onclick = (event) => {
                     return;
                 }
                 firstOperand = firstOperand / secondOperand;
+                break;
+            case '%':
+                firstOperand = firstOperand * (secondOperand / 100);
                 break;
         }
         finish = true;
